50ce63a0636a021baabf72fce71bc9c5725c2b27
Thomas Varghese <v_thomas11@hotmail.com>
2021-02-04 22:07:19 +0000

    corrected unit tests

--- Podium-API-Tests/UserServiceUnitTests.cs
+++ Podium-API-Tests/UserServiceUnitTests.cs
@@ -24,12 +24,22 @@ namespace Podium_API_Tests
             testUser.DateOfBirth = DateTime.Parse("1993-08-10");
             testUser.Id = ObjectId.Parse("601ae6981612b057d41504dd");
 
+            User testUser_under18 = new User();
+            testUser_under18.FirstName = "Joseph";
+            testUser_under18.LastName = "Varghese";
+            testUser_under18.Email = "test2@test.com";
+            testUser_under18.DateOfBirth = DateTime.Parse("2010-05-10");
+            testUser_under18.Id = ObjectId.Parse("601ae6981612b057d41501dd");
+
             var dbContextMock = new Mock<IDatabaseContext>();
             var loggerMock = new Mock<ILogger<UserService>>();
 
 
             dbContextMock.Setup(db => db.FindUser(testUser.Id)).Returns(testUser);
             dbContextMock.Setup(db => db.FindUser(testUser.Email)).Returns(testUser);
+
+            dbContextMock.Setup(db => db.FindUser(testUser_under18.Id)).Returns(testUser_under18);
+            dbContextMock.Setup(db => db.FindUser(testUser_under18.Email)).Returns(testUser_under18);
 
             _userService = new UserService(loggerMock.Object, dbContextMock.Object);
         }
@@ -65,13 +75,23 @@ namespace Podium_API_Tests
         }
 
         [Test]
-        public void InvalidAge()
+        public void CheckValidAge()
         {
             // act
             var res = _userService.ValidAge("601ae6981612b057d41504dd");
 
             // assert
             Assert.True(res);
+        }
+
+        [Test]
+        public void CheckInvalidAge()
+        {
+            // act
+            var res = _userService.ValidAge("601ae6981612b057d41501dd");
+
+            // assert
+            Assert.False(res);
         }
 
         [Test]
